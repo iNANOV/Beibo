b6b0eab9d7e672894a819052118dd56be59895fb
iNANOV <42960169+iNANOV@users.noreply.github.com>
2025-07-01 10:51:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(
-    name='beibo',
-    version='0.1.3',
-    description='🤖 Predict the stock market with AI 用AI预测股票市场',
+    name='beibo-fix', 
+    version='0.1.3-fix',
+    description='🤖 Fork of Beibo: Predict the stock market with AI (includes bug fixes and improvements)',
     py_modules=['beibo'],
     package_dir={'': 'src'},
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/ssantoshp/Beibo',
-    author="Santosh Passoubady",
-    author_email="santoshpassoubady@gmail.com",
+    url='https://github.com/iNANOV/Beibo',  
+    author="iNANOV",
+    author_email="nanov.iliya@gmail.com",
     license='MIT',
     install_requires=[
         'darts',
